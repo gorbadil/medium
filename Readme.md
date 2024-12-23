9014561e653c13b7c9356b371613cf1a812821f1
Ersin Mutlu <gorbadil@192.168.1.13>
2024-12-23 14:02:32 +0300

    daja js added

--- Readme.md
+++ Readme.md
@@ -2,7 +2,7 @@
 
 1. VPS Kurulum
    - Medium makale: [VPS Kurulum](https://medium.com/@gorbadil/güvenli-bir-vps-sunucusu-nasıl-kurulur-daca9dec1ca0)
-   - Markdown: [VPS Kurulum](./VPS_Kurulum.md)
+   - Markdown: [VPS Kurulum](https://github.com/gorbadil/medium/blob/main/VPS_Kurulum.md)
 2. VPS Docker k8s Jenkins Kurulum
    - Medium makale: [VPS Docker k8s Jenkins Kurulum](https://medium.com/@gorbadil/vps-üzerinde-continuous-integration-continuous-deployment-ci-cd-kurulumu-ve-nginx-reverse-proxy-1a4f990cc905)
-   - [VPS Docker k8s Jenkins Kurulum](./VPS_Docker_k8s_Jenkins.md)
+   - [VPS Docker k8s Jenkins Kurulum](https://github.com/gorbadil/medium/blob/main/VPS_Docker_k8s_Jenkins.md)
---
+++ data.js
@@ -0,0 +1,18 @@
+export const data = [
+    {
+        id: 1,
+        name: "VPS Kurulumu",
+        acıklama: "Güvenlik VPS Kurulumu",
+        desc: "Guide for set up VPS server with securily SSH connection and Docker installation on Ubuntu operating system.",
+        mediumTr: "https://medium.com/@gorbadil/güvenli-bir-vps-sunucusu-nasıl-kurulur-daca9dec1ca0",
+        gitMarkdown: "https://github.com/gorbadil/medium/blob/main/VPS_Kurulum.md"
+    },
+    {
+        id: 2,
+        name: "Docker k8s Jenkins",
+        acıklama: "CI/CD için Docker, Kubernetes ve Jenkins Kurulum Rehberi",
+        desc: "Guide for set up Docker, Kubernetes and Jenkins for CI/CD pipeline.",
+        mediumTr: "https://medium.com/@gorbadil/vps-üzerinde-continuous-integration-continuous-deployment-ci-cd-kurulumu-ve-nginx-reverse-proxy-1a4f990cc905",
+        gitMarkdown: "https://github.com/gorbadil/medium/blob/main/VPS_Docker_k8s_Jenkins.md"
+    }
+]
